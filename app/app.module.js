deefb87759f7c832cf030f00a62677d693071385
Risal Shahriar Shefin <risal.shefin@enosisbd.com>
2024-01-08 12:22:30 +0600

    - Updated package.json, app module, index.html. - Added main.ts, tsconfig, systemjs.config.js. - Other necessary changes.

--- app/app.module.js
+++ app/app.module.js
@@ -1,10 +1,25 @@
-'use strict';
-
-// Define the `phonecatApp` module
-angular.module('phonecatApp', [
-  'ngAnimate',
-  'ngRoute',
-  'core',
-  'phoneDetail',
-  'phoneList'
-]);
+import { __decorate, __metadata, __param } from "tslib";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { UpgradeModule } from '@angular/upgrade/static';
+import { Inject } from '@angular/core';
+let AppModule = class AppModule {
+    constructor(upgrade) {
+        this.upgrade = upgrade;
+    }
+    ngDoBootstrap() {
+        this.upgrade.bootstrap(document.documentElement, ['phonecatApp']);
+    }
+};
+AppModule = __decorate([
+    NgModule({
+        imports: [
+            BrowserModule,
+            UpgradeModule,
+        ],
+    }),
+    __param(0, Inject(UpgradeModule)),
+    __metadata("design:paramtypes", [UpgradeModule])
+], AppModule);
+export { AppModule };
+//# sourceMappingURL=app.module.js.map
