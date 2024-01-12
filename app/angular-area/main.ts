22ed49b534ca440c32e0c007b04173a308651db9
Risal Shahriar Shefin <risal.shefin@enosisbd.com>
2024-01-12 19:12:58 +0600

    Working hybrid build
    - Added new webpack config to bundle all the angular js scripts.
    - Updated angular.json to include angular js bundled script and jquery, copy htmls of angular js, and do other necessary tasks.
    - Updated package.json to include new build and serve commands.
    - Updated index.html and moved it to the app directory.
    - Other necessary changes.
    - Cleaned unnecessary files.

--- app/angular-area/main.ts
+++ app/angular-area/main.ts
@@ -1,5 +1,4 @@
-import '@angular/compiler';
-import 'zone.js';
+import 'zone.js'; // Without this, you'll get "Error: NG0908: In this configuration Angular requires Zone.js"
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
