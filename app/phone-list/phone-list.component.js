7ab9a081bec3f913bbf8f7a6fda989e0d384892d
Risal Shahriar Shefin <risal.shefin@enosisbd.com>
2024-01-17 11:05:36 +0600

    Replaced "ng-include src" with "template request" for dynamic loading.

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -4,9 +4,20 @@
 angular.
   module('phoneList').
   component('phoneList', {
-    template: `<ng-include src="'phone-list/phone-list.template.html'"></ng-include>`,
-    controller: ['phone',
-      function PhoneListController(phone) {
+    //template: `<ng-include src="'phone-list/phone-list.template.html'"></ng-include>`,
+    template: '<div id="template-container">Loading...</div>',
+    controller: ['phone', '$scope', '$templateRequest', '$compile',
+      function PhoneListController(phone, $scope, $templateRequest, $compile) {
+
+        var templateUrl = 'phone-list/phone-list.template.html';
+        // Use $templateRequest to load the template asynchronously
+        $templateRequest(templateUrl).then(function(template) {
+          var angularElement = angular.element(template);
+          var angularElementContainer = angular.element(document.getElementById('template-container'));
+          angularElementContainer.empty();
+          angularElementContainer.append($compile(angularElement)($scope));
+        });
+
         phone.query().subscribe(phones => {
           this.phones = phones;
         });
